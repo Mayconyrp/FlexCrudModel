71505baedc0ee62190b415c1973159a178c40b35
Mayconyrp <csa.maycon@gmail.com>
2023-06-06 13:22:25 -0300

    Implementacao de estruturas

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 const DB_NAME = 'nome_do_seu_banco';
-const DB_USER = 'nome_do_seu_user'; //em alguns bancos o padrão é root
-const DB_PASSWORD = 'digite_sua_senha';
+const DB_USER = 'root'; //em alguns bancos o padrão user é root
+const DB_PASSWORD = 'sua_senha';
 
 const conexaodb = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: 'localhost',
--- testemunhas/TestemunhasController.js
+++ testemunhas/TestemunhasController.js
@@ -1,76 +1,101 @@
-const express = require("express")
-const router = express.Router()
-const Testemunha = require("../models/testemunha")
+const express = require("express");
+const router = express.Router();
+const Testemunha = require("../models/testemunha");
 
 
-//API PARA CADASTRAR
-router.post("/enviardados", (req, res) => {
-    const { nome, email, depoimento } = req.body
+// Função para validar os campos obrigatórios
+const validateFields = (nome, email, depoimento) => {
+    if (!nome || !email || !depoimento) {
+        return false;
+    }
+    return true;
+};
 
-    console.log(nome, email, depoimento)
+// API PARA CADASTRAR
+router.post("/depoimento", (req, res) => {
+    const { nome, email, depoimento } = req.body;
+
+    console.log(nome, email, depoimento);
+
+    if (!validateFields(nome, email, depoimento)) {
+        res.status(400).send("Todos os campos são obrigatórios");
+        return;
+    }
 
     Testemunha.create({
         nome: nome,
         email: email,
         depoimento: depoimento,
-    }).then((dadoscadastro) => {
-        console.log(dadoscadastro.toJSON()) // Linha para limpeza de Dados
-        res.send("Dados enviado")
     })
-
-})
-
-//API PARA EXIBIÇÃO DE DADOS
-router.get("/depoimentos", (req, res) => {
-    Testemunha.findAll({
-    }).then((Testemunhas) => {
-        console.log(Testemunhas.map(Testemunhas => Testemunhas.toJSON())) // Linha para limpeza de Dados
-        res.send(Testemunhas)
-    }).catch((error) => {
-        console.log(error)
-    })
-})
-
-//API PARA ATUALIZAR OS DADOS EXISTENTES
-router.put('/depoimentos/update/:id', (req, res) => {
-    const id = req.params.id;
-    const { nome, email, depoimento } = req.body;
-
-    Testemunha.update(
-        { nome: nome, email: email, depoimento: depoimento },
-        { where: { id: id } }
-    )
-        .then(() => {
-            res.status(200).send('Testemunha atualizada com sucesso!');
+        .then((dadoscadastro) => {
+            console.log(dadoscadastro.toJSON()); // Linha para limpeza de Dados
+            res.status(201).send("Dados enviados com sucesso");
         })
         .catch((error) => {
             console.log(error);
-            res.status(500).send('Erro ao atualizar Testemunha');
+            res.status(500).send("Erro ao cadastrar");
         });
 });
 
+
+// API PARA EXIBIÇÃO DE DADOS
+router.get("/depoimentos", (req, res) => {
+    Testemunha.findAll({})
+        .then((Testemunhas) => {
+            console.log(Testemunhas.map((testemunha) => testemunha.toJSON())); // Linha para limpeza de Dados
+            res.status(200).send(Testemunhas);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Erro ao buscar depoimentos");
+        });
+});
+
+// API PARA ATUALIZAR OS DADOS EXISTENTES
+router.put("/depoimento/editar/:id", (req, res) => {
+    const id = req.params.id;
+    const { nome, depoimento } = req.body;
+
+    if (!nome || !depoimento) {
+        res.status(400).send("Nome e depoimento são campos obrigatórios");
+        return;
+    }
+
+    Testemunha.update(
+        { nome: nome, depoimento: depoimento },
+        { where: { id: id } }
+    )
+        .then((result) => {
+            if (result[0] === 0) {
+                res.status(404).send("Testemunha não encontrada");
+            } else {
+                res.status(200).send("Testemunha atualizada com sucesso");
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Erro ao atualizar Testemunha");
+        });
+});
 // API PARA EXCLUSÃO DE DADOS EXISTENTES
-router.delete('/depoimentos/delete/:id', (req, res) => {
+router.delete("/depoimento/:id", (req, res) => {
     const id = req.params.id;
 
     Testemunha.destroy({
-        where: { id: id }
+        where: { id: id },
     })
-        .then(() => {
-            res.status(200).send('Usuário deletado com sucesso!');
-            console.log('Usuario deletado com sucesso!');
+        .then((result) => {
+            if (result === 0) {
+                res.status(404).send("Testemunha não encontrada");
+            } else {
+                res.status(200).send("Testemunha deletada com sucesso");
+                console.log("Usuário deletado com sucesso!");
+            }
         })
         .catch((error) => {
             console.log(error);
-            res.status(500).send('Erro ao deletar Testemunha');
+            res.status(500).send("Erro ao deletar Testemunha");
         });
 });
-
-
-
-
-
-
-
 
 module.exports = router;
